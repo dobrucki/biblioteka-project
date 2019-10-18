da0fbfb38f1f44755c411a62e92350e52f93ff7b
Jedrzej Dobrucki <dobrucki2@gmail.com>
2019-10-18 11:08:23 +0200

    Changed structure.

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,21 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+from config import config
 
 
-app = Flask(__name__)
-bootstrap = Bootstrap(app)
-moment = Moment(app)
+bootstrap = Bootstrap()
+moment = Moment()
+
+
+def create_app(config_name):
+    app = Flask(__name__)
+    app.config.from_object(config[config_name])
+
+    bootstrap.init_app(app)
+    moment.init_app(app)
+
+    from app.main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    return app
